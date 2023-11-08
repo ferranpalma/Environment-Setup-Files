a816af64a5a8a97810f3322751476983a031bb27
ferranpalma <ferranpalmac@gmail.com>
2023-11-08 12:44:54 +0100

    Ensure install lua lsp

--- .config/nvim/lua/ferran/plugins/lsp/mason.lua
+++ .config/nvim/lua/ferran/plugins/lsp/mason.lua
@@ -1,41 +1,40 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        -- import mason
+        local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+        -- import mason-lspconfig
+        local mason_lspconfig = require("mason-lspconfig")
 
-    -- import lspconfig
-    local lspconfig = require("lspconfig")
+        -- import lspconfig
+        local lspconfig = require("lspconfig")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-      }
-    })
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        "gopls",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
+        mason_lspconfig.setup({
+            -- list of servers for mason to install
+            ensure_installed = {
+                "gopls",
+                "lua_ls",
+            },
+            -- auto-install configured servers (with lspconfig)
+            automatic_installation = true, -- not the same as ensure_installed
+        })
 
-    lspconfig.gopls.setup({})
-    lspconfig.lua_ls.setup({})
-
+        lspconfig.gopls.setup({})
+        lspconfig.lua_ls.setup({})
     end,
 }
