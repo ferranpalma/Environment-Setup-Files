bdaf1f3afc46358a60d86e372fa7dedc9138c048
ferranpalma <ferranpalmac@gmail.com>
2024-01-15 08:48:53 +0100

    Display filetree at right

--- .config/nvim/lua/ferran/plugins/nvim-tree.lua
+++ .config/nvim/lua/ferran/plugins/nvim-tree.lua
@@ -14,6 +14,7 @@ return {
         -- configure nvim-tree
         nvimtree.setup({
             view = {
+                side = "right",
                 width = 35,
                 relativenumber = true,
             },
@@ -50,12 +51,13 @@ return {
         })
 
         -- set keymaps
-        local keymap = vim.keymap                                                                                       -- for conciseness
+        local keymap = vim
+            .keymap                                                                                     -- for conciseness
 
-        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })                     -- toggle file explorer
+        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })     -- toggle file explorer
         keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>",
-            { desc = "Toggle file explorer on current file" })                                                          -- toggle file explorer on current file
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })                 -- collapse file explorer
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })                   -- refresh file explorer
+            { desc = "Toggle file explorer on current file" })                                          -- toggle file explorer on current file
+        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })   -- refresh file explorer
     end,
 }
