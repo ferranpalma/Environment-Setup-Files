50df7b49fa96753cadc760b817156bd98f2ffc2e
ferranpalma <ferranpalmac@gmail.com>
2024-05-16 12:19:42 +0200

    remove duplicated treesitter

--- .config/nvim/lua/ferran/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/ferran/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { 'c', 'go', 'lua', 'python', 'rust', 'javascript', 'typescript', 'vue' },
+            ensure_installed = { 'c', 'go', 'lua', 'python', 'rust', 'html', 'css', 'javascript' },
             auto_install = true,
         })
     end
